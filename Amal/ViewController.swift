2156b63ae610f821e0aa93f3bfd6cb6cfff7b2cc
choym2015 <choym2015@gmail.com>
2026-04-25 17:08:38 -0400

    Initial Commit

---
+++ Amal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amal
+//
+//  Created by 조영민 on 4/25/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
